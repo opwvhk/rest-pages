ed4ec0f03e2b5dcc4db4d5caa76e594b2dbfc1ec
Oscar Westra van Holthe - Kind <opwvhk@users.noreply.github.com>
2012-11-23 20:14:50 +0000

    Added copyright notice. The copyright already applied as per the POM, but now all files reflect it.

--- src/main/java/net/sf/opk/rest/forms/conversion/Converter.java
+++ src/main/java/net/sf/opk/rest/forms/conversion/Converter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Oscar Westra van Holthe - Kind
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
 package net.sf.opk.rest.forms.conversion;
 
 import java.util.List;
@@ -14,7 +29,20 @@ import com.fasterxml.classmate.ResolvedType;
  */
 public interface Converter
 {
+	/**
+	 * Determine whether the converter can convert to the specified type.
+	 *
+	 * @param resolvedType the type to convert to
+	 * @return {@code true} if the converter can convert to the type; {@code false} otherwise
+	 */
 	boolean canConvertTo(ResolvedType resolvedType);
 
+	/**
+	 * Convert the form values to the given type. All values belong to the same field.
+	 *
+	 * @param resolvedType the type to convert to
+	 * @param values       the values to convert
+	 * @return the converted value
+	 */
 	<T> T convertTo(ResolvedType resolvedType, List<String> values);
 }
--- src/main/java/net/sf/opk/rest/pages/ResourceForwardWriter.java
+++ src/main/java/net/sf/opk/rest/pages/ResourceForwardWriter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Oscar Westra van Holthe - Kind
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
 package net.sf.opk.rest.pages;
 
 import java.io.IOException;
@@ -40,9 +55,14 @@ public class ResourceForwardWriter implements MessageBodyWriter<ResourceForward>
 
 
 	/**
-	 * Delegates forwarding the request to {@link ResourceForwardFilter}. This is required because the actual request
-	 * and response objects are required by a {@link RequestDispatcher}. This is not possible with CDI or JAX-RS, as
-	 * they supply a proxy. A filter provides the required access to the request and response.
+	 * <p>Delegates forwarding the request to {@link ResourceForwardFilter}. This is required because the actual request
+	 * and response objects are required by a {@link RequestDispatcher}.</p>
+	 *
+	 * <p>This is not possible with CDI or (vanilla) JAX-RS, as they supply a proxy. Injecting a {@code
+	 * ThreadLocal&lt;HttpServletRequest&gt;} and {@code ThreadLocal&lt;HttpServletRequest&gt;} is a Jersey extension, and
+	 * thus not portable.</p>
+	 *
+	 * <p>The filter used does provide a standards compliant way to access the request and response objects.</p>
 	 *
 	 * @param resourceForward the {@code ResourceForward} to handle
 	 * @param type            always {@code ResourceForward.class}
@@ -53,6 +73,10 @@ public class ResourceForwardWriter implements MessageBodyWriter<ResourceForward>
 	 * @param output          the content stream (ignored)
 	 * @throws IOException             when the {@code ResourceForwardFilter} cannot forward the request
 	 * @throws WebApplicationException when the {@code ResourceForwardFilter} cannot forward the request
+	 * @see <a href="http://jersey.576304.n2.nabble.com/Thread-access-to-Context-objects-tp3880758p3880915.html">Jersey
+	 *      mailing list: Thread-access-to-Context-objects</a>
+	 * @see <a href="http://tech.lefedt.de/2010/11/an-mvc-extension-for-jersey-selecting-views-based-on-executed-controller-and-action">
+	 *     Blog entry demonstrating an MVC extension for Jersey</a>
 	 */
 	@Override
 	public void writeTo(ResourceForward resourceForward, Class<?> type, Type genericType, Annotation[] annotation,
