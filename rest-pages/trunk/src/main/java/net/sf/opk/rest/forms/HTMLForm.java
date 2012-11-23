e0580a1e841a58bfffd8d6443fc4b776548f7819
okind <okind@f97979a5-642f-4712-bc61-5aec2407c52e>
2012-11-23 20:14:50 +0000

    Added copyright notice. The copyright already applied as per the POM, but now all files reflect it.

--- rest-pages/trunk/src/main/java/net/sf/opk/rest/forms/HTMLForm.java
+++ rest-pages/trunk/src/main/java/net/sf/opk/rest/forms/HTMLForm.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Oscar Westra van Holthe - Kind
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and limitations under the
+ * License.
+ */
 package net.sf.opk.rest.forms;
 
 import java.util.AbstractMap;
@@ -192,13 +207,14 @@ public class HTMLForm
 		{
 			prefix += '.';
 		}
+		int prefixLength = prefix == null ? 0 : prefix.length();
 
 		for (Map.Entry<String, List<String>> formParameter : formData.entrySet())
 		{
 			String parameterName = formParameter.getKey();
 			if (prefix == null || parameterName.startsWith(prefix))
 			{
-				BeanProperty property = propertyParser.parse(parameterName);
+				BeanProperty property = propertyParser.parse(parameterName.substring(prefixLength));
 				Object value = conversionService.convert(formParameter.getValue(), property.getType(bean));
 				property.setValue(bean, value);
 			}
